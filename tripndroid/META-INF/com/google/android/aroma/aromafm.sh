fc0ebe53e9b5b0ed4784f1388fcc123b316d3c56
Paul <javelinanddart@gmail.com>
2015-01-26 17:50:44 -0600

    Remove Irrelevant Files for Smaller Zip
    Signed-off-by: Paul <javelinanddart@gmail.com>

--- tripndroid/META-INF/com/google/android/aroma/aromafm.sh
+++
@@ -1,6 +0,0 @@
-#!/sbin/sh
-# Save Zip Location
-echo "ziplocation=${UPDATE_PACKAGE}" > /tmp/aroma/aromafm.prop
-
-# Copy Configuration file
-cp /tmp/aroma/aromafm.cfg `echo "${UPDATE_PACKAGE}.cfg"`
--- tripndroid/META-INF/com/google/android/aroma/scripts/save.sh
+++
@@ -1,9 +0,0 @@
-#!/sbin/sh
-mount /system;
-if [ ! -d /system/aroma ]; then
-	mkdir /system/aroma;
-fi
-cp -f /tmp/aroma-data/*.prop /system/aroma/;
-rm -rf /system/aroma/installation_type.prop;
-umount /system;
-exit 0
